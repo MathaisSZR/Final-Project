d2493f83d44252c675fc17e5a4e14e0f59743b59
Mathias Saenz de Zumaran Rodirguez <32782296+MathaisSZR@users.noreply.github.com>
2017-10-13 19:23:00 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$("#fondo").css({"height":$(window).height() + "px"});
+
+
+	$(window).scroll(function(){
+		scroll = $(window).scrollTop();
+
+			var barscr = $(window).scrollTop();
+			var position = barscr * 0.05;
+			var scroll;
+			var flag = false;
+			
+			$("#body").css({
+			"background-position": "0 -" + position +"px"
+		});
+
+			if (scroll > 100) {
+				if (!flag) {
+					$("#logo").css({"margin-top": "250%", "max-width": "100px", "max_height": "100px", "margin-left": "25px"});
+					flag = true;
+				}
+			}
+			else{
+				if (!flag) {
+					$("#logo").css({"margin-top": "0%", "max-width": "500px", "max_height": "500px", "margin-left": "300%"});
+					flag = true;
+				}
+		}
+	});
+});
